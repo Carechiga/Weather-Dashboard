7555a07a783afca2129267abe90259cbac538b80
Chris <carechiga43@gmail.com>
2023-01-31 22:04:30 -0800

    added states and countries to drop down selectors

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Week-Six-Challenge
 Weather Dashboard Challenge
+## Resources
+The list of country codes for the select element was obtained here: https://www.freeformatter.com/iso-country-list-html-select.html
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var requestURL = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&a
 var geocodingURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=3appid=792edf62aceac617f83db5acead1d197'
 
 function getWeather(){
-fetch()
+    var cityName = city;
+    var stateCode = state;
+    var geocodingURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + stateCode + '&limit=1appid=792edf62aceac617f83db5acead1d197';
+fetch(geocodingURL)
 .then(function(response){
     return response.json();
 }).then(function(data){
