803828e7eec5a7cd6b8e37873cc14831dd93d326
Chris <carechiga43@gmail.com>
2023-02-02 14:21:15 -0800

    added current day weather, current/weekly format

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var stateCode = document.getElementById('state-select');
 var countryCode = document.getElementById('country-code');
 var latitude = document.getElementById('lat');
 var longitude = document.getElementById('lon');
-var currentWeatherContainer = document.getElementById('todays-weather');
+var currentWeatherContainer = document.getElementById('currentWeather');
 var weeklyWeatherContainer = document.getElementById('weeklyForecast');
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
@@ -40,16 +40,74 @@ function getWeatherLatLon(event){
     console.log(data);
 
     //generate weekly forecast day-cards
+    var WFHeader = document.createElement('h3');
+    WFHeader.classList.add('custom-center');
+    WFHeader.textContent = "Weekly Forecast";
+    weeklyWeatherContainer.append(WFHeader);
+    
     for(let i = 1; i < 6; i++){
     var dt = data.list[(i * 8) - 1].dt;
     var date = new Date(dt * 1000);
    
+    var displayWeeklyWeather = document.createElement('div'); 
+    displayWeeklyWeather.classList.add('container-fluid', 'custom-container', 'col');
+    weeklyWeatherContainer.append(displayWeeklyWeather);
+  
+    var weeklyWeatherContent = document.createElement('div');
+    weeklyWeatherContent.classList.add('card', 'customCard,', 'text-primary-emphasis', 'bg-primary-subtle', 'border', 'border-primary-subtle');
+    displayWeeklyWeather.append(weeklyWeatherContent);
+   
+    var cardBodyWeekly = document.createElement('div');
+    cardBodyWeekly.classList.add('card-body');
+    weeklyWeatherContent.append(cardBodyWeekly);
+
+    var weatherIconWeekly = document.createElement('img');
+    weatherIconWeekly.src = 'http://openweathermap.org/img/wn/'+ data.list[i].weather[0].icon + '@2x.png';
+
+    var titleElWeekly = document.createElement('h3');
+    titleElWeekly.classList.add('card-title');
+    titleElWeekly.textContent = date.toDateString() + " - " + data.city.name;
+  
+    var cardTextWeekly = document.createElement('h4');
+    cardTextWeekly.classList.add('card-text');
+    cardTextWeekly.textContent = data.list[i].main.temp + "°F";
+  
+    cardBodyWeekly.append(titleElWeekly, weatherIconWeekly, cardTextWeekly);
+
+    var weatherListWeekly = document.createElement('ul');
+    weatherListWeekly.classList.add('list-group', 'list-group-flush');
+    weeklyWeatherContent.append(weatherListWeekly);
+
+    var feelWeekly = document.createElement('li');
+    feelWeekly.classList.add('list-group-item');
+    feelWeekly.textContent = 'Feels like: ' + data.list[i].main.feels_like + "°F";
+
+    var humidityWeekly = document.createElement('li');
+    humidityWeekly.classList.add('list-group-item');
+    humidityWeekly.textContent = 'Humidity: ' + data.list[i].main.humidity + "%";
+
+    var windWeekly = document.createElement('li');
+    windWeekly.classList.add('list-group-item');
+    windWeekly.textContent = 'Wind: ' + data.list[i].wind.speed + " mph";
+
+    weatherListWeekly.append(feelWeekly, humidityWeekly, windWeekly);
+    }
+
+    // creates 
+    var CWHeader = document.createElement('h2');
+    CWHeader.classList.add('custom-center');
+    CWHeader.textContent = "Current Weather";
+    currentWeatherContainer.append(CWHeader);
+    
+    var currentDT = data.list[0].dt;
+    var currentDate = new Date(currentDT * 1000);
+   
     var displayCurrentWeather = document.createElement('div'); 
-    displayCurrentWeather.classList.add('container-fluid', 'row', 'custom-container', 'col');
-    weeklyWeatherContainer.append(displayCurrentWeather);
+    displayCurrentWeather.classList.add('container-fluid', 'custom-container', 'col');
+    currentWeatherContainer.append(displayCurrentWeather);
   
     var currentWeatherContent = document.createElement('div');
-    currentWeatherContent.classList.add('card', 'customCard');
+    currentWeatherContent.classList.add('card', 'customCard,', 'text-primary-emphasis', 'bg-primary-subtle', 'border', 'border-primary-subtle');
     displayCurrentWeather.append(currentWeatherContent);
    
     var cardBody = document.createElement('div');
@@ -57,15 +115,15 @@ function getWeatherLatLon(event){
     currentWeatherContent.append(cardBody);
 
     var weatherIcon = document.createElement('img');
-    weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data.list[i].weather[0].icon + '@2x.png';
+    weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '@4x.png';
 
     var titleEl = document.createElement('h3');
     titleEl.classList.add('card-title');
-    titleEl.textContent = date.toDateString() + " - " + data.city.name;
+    titleEl.textContent = currentDate.toDateString() + " - " + data.city.name;
   
     var cardText = document.createElement('h4');
     cardText.classList.add('card-text');
-    cardText.textContent = data.list[i].main.temp + "°F";
+    cardText.textContent = data.list[0].main.temp + "°F";
   
     cardBody.append(titleEl, weatherIcon, cardText);
 
@@ -75,19 +133,17 @@ function getWeatherLatLon(event){
 
     var feel = document.createElement('li');
     feel.classList.add('list-group-item');
-    feel.textContent = 'Feels like: ' + data.list[i].main.feels_like + "°F";
+    feel.textContent = 'Feels like: ' + data.list[0].main.feels_like + "°F";
 
     var humidity = document.createElement('li');
     humidity.classList.add('list-group-item');
-    humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + "%";
+    humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + "%";
 
     var wind = document.createElement('li');
     wind.classList.add('list-group-item');
-    wind.textContent = 'Wind: ' + data.list[i].wind.speed + " mph";
+    wind.textContent = 'Wind: ' + data.list[0].wind.speed + " mph";
 
-    weatherList.append(feel, humidity, wind);
-    }}
-    )
+    weatherList.append(feel, humidity, wind);})
 }
 
 
