b656204f69ff23e9040dc79370377e67030e1631
Chris <carechiga43@gmail.com>
2023-02-03 12:24:20 -0800

    recent history buttons now pull up weather when clicked, README updated

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Week-Six-Challenge
 Weather Dashboard Challenge
+
+## Project Description
+For this project the intent was to create a funcitonin weather dashboard that displays the Current weather conditions for a selected city as well as weather conditions for the upcoming 5 days. The purpose was to demonstrate knowledge  calling a 3rd party API (openWeatherMap) to get weather conditions from a user input. For my application the user is able to input a desired City and country (and can add State if relevant) to bring up the desired results. The city/state/country-code is translated into latitude/longitude cooridantes which we then use to call the API. It was also designed to store search history to local data in order to easily recall weather for City that was viewed before.
+
+![alt text](assets/images/Screenshot.png)
+
 ## Resources
 The list of country codes for the select element was obtained here: https://www.freeformatter.com/iso-country-list-html-select.html
+
+## Deployed Application
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var stateCode = document.getElementById('state-select');
 var countryCode = document.getElementById('country-code');
 
 var currentWeatherContainer = document.getElementById('currentWeather');
-var weeklyWeatherContainer = document.getElementById('weeklyForecast');
 var currentWeatherIcon = document.getElementById('currentWeatherIcon');
 var currentCardTitle = document.getElementById('currentCardTitle');
 var currentCardText = document.getElementById('currentCardText');
@@ -13,6 +12,22 @@ var currentHumidity = document.getElementById('currentHumidity');
 var currentwind = document.getElementById('currentWind');
 var currentVisibility = document.getElementById('currentVisibility');
 
+var weeklyWeatherContainer = document.getElementById('weeklyForecast');
+var weeklyIcon = document.querySelectorAll('.weeklyWeatherIcon');
+var weeklyCardTitle = document.querySelectorAll('.weeklyCardTitle');
+var weeklyCardText = document.querySelectorAll('.weeklyCardText');
+var weeklyFeel = document.querySelectorAll('.weeklyFeel');
+var weeklyHumidity = document.querySelectorAll('.weeklyHumidity');
+var weeklyWind = document.querySelectorAll('.weeklyWind');
+
+var cityHistory = document.getElementById('cityHistoryDiv');
+
+// this array is for calling on local storage to load saved cities when the app is first opened, limited to the 10 most recent searches
+var localStorageArray = ['obj1', 'obj2', 'obj3', 'obj4', 'obj5', 'obj6', 'obj7', 'obj7', 'obj8', 'obj9', 'obj10'];
+var storageIndex = 0;
+
+
+//this function takes data from the weather API and puts it into the appropriate HTML elements for the current day
 function writeCurrentWeather(data){
         var currentDT = data.list[0].dt;
         var currentDate = new Date(currentDT * 1000);
@@ -20,21 +35,62 @@ function writeCurrentWeather(data){
         currentVisibility.classList.remove("custom-hidden");
         currentVisibility.classList.add("custom-visible");
         currentWeatherIcon.src = 'http://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '@4x.png';
-        currentCardTitle.textContent = currentDate.toDateString() + " - " + data.city.name;
-        currentCardText.textContent = data.list[0].main.temp + "°F";
+        currentCardTitle.textContent = currentDate.toDateString();
+        currentCardText.textContent = data.city.name + " - " + data.list[0].main.temp + "°F";
         currentFeelsLike.textContent = 'Feels like: ' + data.list[0].main.feels_like + "°F";
         currentHumidity.textContent = 'Humidity: ' + data.list[0].main.humidity + "%";
         currentWind.textContent = 'Wind: ' + data.list[0].wind.speed + " mph";
 }
 
+//this function takes data from the weather API and puts it into the appropriate HTML elements for the Weekly Forecast
 function writeWeeklyWeather(data){
+    weeklyVisibility.classList.remove("custom-hidden");
+    weeklyVisibility.classList.add("custom-visible");
+      
+    for(let i = 1; i < 6; i++){
+    var dt = data.list[(i * 8) - 1].dt;
+    var date = new Date(dt * 1000);
+  
+    weeklyIcon[i - 1].src = 'http://openweathermap.org/img/wn/'+ data.list[i].weather[0].icon + '@2x.png';
+    weeklyCardTitle[i -1].textContent = date.toDateString();
+    weeklyCardText[i - 1].textContent = data.city.name + " - " + data.list[i].main.temp + "°F";
+    weeklyFeel[i - 1].textContent = 'Feels like: ' + data.list[i].main.feels_like + "°F";
+    weeklyHumidity[i - 1].textContent = 'Humidity: ' + data.list[i].main.humidity + "%";
+    weeklyWind[i - 1].textContent = 'Wind: ' + data.list[i].wind.speed + " mph";
+    }
+}
+
+//this button creates a button for a recenetly searched Location and appends it to the docuement
+function createHistoryButton(object){
+    var cityButton = document.createElement('button');
+    cityButton.classList.add('btn', 'btn-primary');
+    cityButton.setAttribute('type', 'submit');
+    cityButton.setAttribute('style', 'margin: 5px');
+    cityButton.setAttribute('data-lat', object.lat);
+    cityButton.setAttribute('data-lon', object.lon);
+    cityButton.textContent = object.name;
+    cityHistory.append(cityButton);
+
 
 }
 
-function storeCityData(){
+//this function takes appropriate API URL and returns the info and writes it to the page for the entered location
+function fetchWeather (URL){
+    fetch(URL)
+    .then(function(response){
+    return response.json();
+    }).then(function(data){
+    console.log(data);
 
+    //generate weekly forecast day-cards
+    writeWeeklyWeather(data);
+
+    // creates current Weather card
+    writeCurrentWeather(data);
+    })  
 }
 
+//This function triggers the fetch weather when the get weather button is clicked and uses the selected data to create the API call
 function getWeatherCity(event) {
 
     //this takes city input and retrieves latitude and longitude from API
@@ -44,7 +100,7 @@ function getWeatherCity(event) {
         var countrySpan = "," + countryCode.value;      
         var geocodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + citySpan + stateSpan + countrySpan + '&limit=1&appid=792edf62aceac617f83db5acead1d197';
         console.log(geocodingURL);
-
+    
     fetch(geocodingURL)
         .then(function(response){
         return response.json();
@@ -52,80 +108,54 @@ function getWeatherCity(event) {
         console.log(data);
         var latitude = data[0].lat;
         var longitude = data[0].lon;
+        
+        const latLonObj = {
+            name: cityName.value,
+            lat: latitude,
+            lon: longitude
+        }
+        //sets city name + lat and lon to local storage
+                localStorage.setItem(localStorageArray[storageIndex], JSON.stringify(latLonObj));
+                if(storageIndex < 10){
+                storageIndex++;}
+                else{
+                    storageIndex = 0;
+                }
+
+        createHistoryButton(latLonObj);
      
        //creates a url string with latitude and longitude values and uses to fetch weather
        
         var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=792edf62aceac617f83db5acead1d197';
         console.log(weatherURL);
 
-        fetch(weatherURL)
-        .then(function(response){
-        return response.json();
-        }).then(function(data){
-        console.log(data);
-
-        //generate weekly forecast day-cards
-
-        var WFHeader = document.createElement('h3');
-        WFHeader.classList.add('custom-center');
-        WFHeader.textContent = "Weekly Forecast";
-        weeklyWeatherContainer.append(WFHeader);
-    
-        for(let i = 1; i < 6; i++){
-        var dt = data.list[(i * 8) - 1].dt;
-        var date = new Date(dt * 1000);
-   
-        var displayWeeklyWeather = document.createElement('div'); 
-        displayWeeklyWeather.classList.add('container-fluid', 'custom-container', 'col');
-        weeklyWeatherContainer.append(displayWeeklyWeather);
-  
-        var weeklyWeatherContent = document.createElement('div');
-        weeklyWeatherContent.classList.add('card', 'customCard,', 'text-primary-emphasis', 'bg-primary-subtle', 'border', 'border-primary-subtle');
-        displayWeeklyWeather.append(weeklyWeatherContent);
-   
-        var cardBodyWeekly = document.createElement('div');
-        cardBodyWeekly.classList.add('card-body');
-        weeklyWeatherContent.append(cardBodyWeekly);
-
-        var weatherIconWeekly = document.createElement('img');
-        weatherIconWeekly.src = 'http://openweathermap.org/img/wn/'+ data.list[i].weather[0].icon + '@2x.png';
-
-        var titleElWeekly = document.createElement('h3');
-        titleElWeekly.classList.add('card-title');
-        titleElWeekly.textContent = date.toDateString() + " -- " + data.city.name;
-  
-        var cardTextWeekly = document.createElement('h4');
-        cardTextWeekly.classList.add('card-text');
-        cardTextWeekly.textContent = data.list[i].main.temp + "°F";
-  
-        cardBodyWeekly.append(titleElWeekly, weatherIconWeekly, cardTextWeekly);
-
-        var weatherListWeekly = document.createElement('ul');
-        weatherListWeekly.classList.add('list-group', 'list-group-flush');
-        weeklyWeatherContent.append(weatherListWeekly);
-
-        var feelWeekly = document.createElement('li');
-        feelWeekly.classList.add('list-group-item');
-        feelWeekly.textContent = 'Feels like: ' + data.list[i].main.feels_like + "°F";
-
-        var humidityWeekly = document.createElement('li');
-        humidityWeekly.classList.add('list-group-item');
-        humidityWeekly.textContent = 'Humidity: ' + data.list[i].main.humidity + "%";
-
-        var windWeekly = document.createElement('li');
-        windWeekly.classList.add('list-group-item');
-        windWeekly.textContent = 'Wind: ' + data.list[i].wind.speed + " mph";
-
-        weatherListWeekly.append(feelWeekly, humidityWeekly, windWeekly);
-        }
-
-        // creates current Weather card
-        writeCurrentWeather(data);
-        })     
+     fetchWeather(weatherURL)  
     }) 
-    }
+}
 
+//this function calls the API with the data stored in the history buttons
+function getWeatherHistory(event){
+    event.preventDefault;
+    var latitude = event.target.getAttribute('data-lat');
+    var longitude = event.target.getAttribute('data-lon');
+    var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=792edf62aceac617f83db5acead1d197';
+        console.log(weatherURL);
+  fetchWeather(weatherURL);      
 
+}
+
+// this will load up to 10 recent searches as buttons on load of the page
+function loadRecents (){
+        for(let i = 0; i < localStorageArray.length; i++){
+        
+            var temp = JSON.parse(localStorage.getItem(localStorageArray[i]));
+            if(temp){
+                console.log(temp)
+            createHistoryButton(temp);}
+        }
+}
+
+loadRecents();
 
     getWeatherBtn.addEventListener("click", getWeatherCity);
-    
+    cityHistory.addEventListener("click", getWeatherHistory);
